3935f2ff1c1627d3706a981d053393abd2042833
Alexey Zelenskiy <zelenskii1999@mail.ru>
2019-11-07 01:36:57 +0700

    first commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import './app-header.css';
+const Header = () => {
+        return (
+            <div className="app-header d-flex">
+                <h1>Alexey Zelenskiy</h1>
+                <h2>5 записей, из них понравилось 0</h2>
+            </div>
+        )
+};
+export default Header;
