27860af53ba71179e567ab4ed29d905ca04bba45
Alexey Zelenskiy <zelenskii1999@mail.ru>
2019-11-07 17:18:03 +0700

    lesson-04 easy

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,7 +7,19 @@ import PostList from "../post-list";
 import PostAddForm from "../post-add-form";
 
 import './app.css'
+
 const App = () => {
+
+    const data = [
+        0,
+        [],
+        undefined,
+        false,
+        {label: 'Going to learn React', important: true, id: 's'},
+        {label: 'This is so good', important: false, id:'d'},
+        {label: 'I need a break...', important: false,id:'v'}
+    ];
+
     return (
         <div className="app">
             <AppHeader/>
@@ -15,9 +27,9 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
-}
+};
 export default App;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import './post-list.css';
 
 import PostListItem from '../post-list-item'
-const PostList = () => {
-    return (
-        <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
-        </ul>
-    )
-};
-export default PostList;
+
+export default class PostList extends Component {
+    constructor(props) {
+        super(props);
+        this.isObject = this.isObject.bind(this);
+    }
+
+    isObject(val) {
+        return val != null && val.constructor.name === 'Object';
+    }
+
+    render() {
+        const {posts} = this.props;
+        const elements = posts.filter(el => {
+            return this.isObject(el)
+        }).map((item) => {
+            const {id, ...itemProps} = item;
+            return (
+                <li key={id} className="list-group-item">
+                    <PostListItem {...itemProps}/>
+                </li>
+            )
+        });
+        return (
+            <ul className="app-list list-group">
+                {elements}
+            </ul>
+        )
+    }
+}
